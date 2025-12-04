6cf133b4fd48f40386c51926ac42c82324d0f543
Daniel Casasola <casasolad86@gmail.com>
2025-12-04 13:00:12 -0600

    add vite-plugin-vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,10 @@ export default defineConfig({
         usePolling: true
       }
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      vueDevTools()
+    ],
     css: {
         preprocessorOptions: {
             scss: {
